e6325de65a5a0e864306105569e7ddf194f36b0f
MichalLementowicz <michal.lementowicz@gmail.com>
2024-05-05 17:51:45 +0200

    zad2

--- Main.java
+++ Main.java
@@ -5,12 +5,12 @@ import company.abstracts.Employee;
 
 public class Main {
     public static void main(String[] args) {
-        Worker worker1 = new Worker("Jan", 2500, 1, "2022-01-01", "Junior Developer");
-        Worker worker2 = new Worker("Pawel", 2700, 2, "2023-03-15", "Senior Developer");
-        Worker worker3 = new Worker("Piotr", 2900, 3, "2020-12-10", "Tester");
-        Worker worker4 = new Worker("Anna", 2100, 4, "2021-06-20", "Project Manager");
+        Worker worker1 = new Worker("Jan", 2500, 1, "2020-03-04", "Junior Developer");
+        Worker worker2 = new Worker("Pawel", 2700, 2, "2022-02-18", "Senior Developer");
+        Worker worker3 = new Worker("Piotr", 2900, 3, "2019-07-10", "Tester");
+        Worker worker4 = new Worker("Anna", 2100, 4, "2019-03-22", "Project Manager");
 
-        Manager manager = new Manager("Michal", 5500, 5, "2019-05-10", "Department Head");
+        Manager manager = new Manager("Michal", 5500, 5, "2018-11-27", "Department Head");
 
         ArrayList<Employee> employees = new ArrayList<>();
         employees.add(worker1);

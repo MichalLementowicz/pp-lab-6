e02261258f278b1bbe0d23e076b02db42a76e8c5
MichalLementowicz <michal.lementowicz@gmail.com>
2024-05-05 18:30:27 +0200

    zad2

--- Main.java
+++ Main.java
@@ -1,24 +1,30 @@
+import java.util.ArrayList;
 import company.models.Manager;
 import company.models.Worker;
+import company.abstracts.Employee;
 
 public class Main {
     public static void main(String[] args) {
         Worker worker1 = new Worker("Jan", 2500, 1, "2020-03-04", "Junior Developer");
         Worker worker2 = new Worker("Pawel", 2700, 2, "2022-02-18", "Senior Developer");
         Worker worker3 = new Worker("Piotr", 2900, 1, "2019-07-10", "Tester");
+        Worker worker3 = new Worker("Anna", 2100, 4, "2019-07-10", "Project manager");
+
         Manager manager = new Manager("Michal", 5500, 5, "2018-11-27", "Department Head");
 
+        ArrayList<Employee> employees = new ArrayList<>();
+        employees.add(worker1);
+        employees.add(worker2);
+        employees.add(worker3);
+        employees.add(worker4);
+        employees.add(manager);
 
-
-        
-        System.out.println(worker1.getName() + " has code: " + worker1.hashCode());
-        System.out.println(worker2.getName() + " has code: " + worker2.hashCode());
-        System.out.println(worker3.getName() + " has code: " + worker3.hashCode());
-        System.out.println(manager.getName() + " has code: " + manager.hashCode());
-
-     
-        System.out.println(worker3.getName() + " equals worker1: " + worker3.equals(worker1));
-        System.out.println(worker3.getName() + " equals worker2: " + worker3.equals(worker2));
-        System.out.println(worker3.getName() + " equals manager: " + worker3.equals(manager));
+        for (Employee emp : employees) {
+            System.out.println("- " + emp.getName() + " (ID: " + emp.getId() +
+                    ", Position: " + emp.getPosition() +
+                    ", Hire date: " + emp.getHireDate() +
+                    ", Salary: " + emp.getSalary() + ")");
+            emp.work();
+        }
     }
 }
